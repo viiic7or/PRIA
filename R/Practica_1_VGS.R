7db9aaa5caf8bd0ac0d0c60da895565ab249dfae
Victor <victorgarciaserrano03@gmail.com>
2023-12-02 19:58:47 +0100

    practica 1

--- R/Practica_1_VGS.R
+++ R/Practica_1_VGS.R
@@ -1,1 +1,13 @@
 #Practica 1
+
+#Generamos un vector con los números del 1 al 50
+
+v1_50 <- 1:50
+print(v1_50)
+
+#Lo repetimos usando seq()
+v1_50_seq <- seq(v1_50)
+
+#Hacemos lo mismo pero lo incrementamos en 0.5 usando by
+v1_50h <- seq(1, 50, by=0.5)
+print(v1_50h)
